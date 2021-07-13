73a9a65e0d68c1cfff267a0a49417c71f1949704
Jenkin-T <jenkin603@gmail.com>
2021-07-13 19:15:40 -0400

    finish up the NavBar

--- my_profile/src/App.js
+++ my_profile/src/App.js
@@ -1,8 +1,26 @@
-
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Navbar from './Components/NavBar';
+import Home from './Contents/Home';
+import About from './Contents/About';
+import Education from './Contents/Education';
+import Skills from './Contents/Skills';
+import Contact from './Contents/Contact';
+
+
 
 function App() {
-  return 1+1;
-}
-
-export default App;
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+    </Router>
+    );
+  }
+  
+  export default App;
